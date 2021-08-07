13d7ebf247ccce6ca7df36d11fa7bd43d645a668
kobayashi-m42 <m42kobayashi@gmail.com>
2021-08-07 15:09:34 +0900

    migration に必要となる値をSSMパラメータストアに追加

--- modules/aws/ecs-migration/variables.tf
+++ modules/aws/ecs-migration/variables.tf
@@ -5,3 +5,19 @@ variable "vpc_id" {
 variable "migration_name" {
   type = string
 }
+
+variable "db_hostname" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
--- providers/aws/environments/prod/22-migration/variables.tf
+++ providers/aws/environments/prod/22-migration/variables.tf
@@ -1,3 +1,15 @@
 locals {
   migration_name = "lgtm-cat-migration"
+  db_password    = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["db_app_password"]
+  db_username    = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["db_app_user"]
+  db_name        = "lgtm_cat"
+  db_hostname    = "lgtm-cat-rds.stg"
+}
+
+data "aws_secretsmanager_secret" "secret" {
+  name = "/prod/lgtm_cat"
+}
+
+data "aws_secretsmanager_secret_version" "secret" {
+  secret_id = data.aws_secretsmanager_secret.secret.id
 }
