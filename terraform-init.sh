695bcc36ac4e8f4d30399adde7c3003cd9db92cf
keitakn <keita.koga.work@gmail.com>
2021-09-17 11:55:15 +0900

    :art: #43 terraform init を実行するスクリプトを最新化

--- terraform-init.sh
+++ terraform-init.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
 tfstateDirList='
-/data/providers/aws/environments/prod/10-acm
-/data/providers/aws/environments/prod/11-images
-/data/providers/aws/environments/prod/12-vercel
-/data/providers/aws/environments/prod/13-txt
-/data/providers/aws/environments/prod/14-iam
-/data/providers/aws/environments/prod/15-ses
-/data/providers/aws/environments/prod/16-cognito
-/data/providers/aws/environments/stg/10-acm
-/data/providers/aws/environments/stg/11-images
-/data/providers/aws/environments/stg/16-cognito
+/data/providers/aws/environments/prod/10-network
+/data/providers/aws/environments/prod/11-acm
+/data/providers/aws/environments/prod/12-images
+/data/providers/aws/environments/prod/13-vercel
+/data/providers/aws/environments/prod/14-txt
+/data/providers/aws/environments/prod/15-iam
+/data/providers/aws/environments/prod/16-ses
+/data/providers/aws/environments/prod/17-cognito
+/data/providers/aws/environments/prod/22-migration
+/data/providers/aws/environments/prod/23-rds
+/data/providers/aws/environments/stg/11-acm
+/data/providers/aws/environments/stg/12-images
+/data/providers/aws/environments/stg/17-cognito
+/data/providers/aws/environments/stg/20-api
+/data/providers/aws/environments/stg/21-lambda-securitygroup
 '
 
 for tfstateDir in ${tfstateDirList}; do
