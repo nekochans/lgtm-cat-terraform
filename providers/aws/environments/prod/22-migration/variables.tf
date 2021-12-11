d1ceb590e9f9a13cb7c407d68175150ee31eb030
kobayashi-m42 <m42kobayashi@gmail.com>
2021-12-12 00:23:48 +0900

    データベース名を SecretManager から取得するように変更

--- providers/aws/environments/prod/22-migration/variables.tf
+++ providers/aws/environments/prod/22-migration/variables.tf
@@ -3,7 +3,7 @@ locals {
   migration_name = "${local.env}-lgtm-cat-migration"
   db_password    = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["db_app_password"]
   db_username    = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["db_app_user"]
-  db_name        = "lgtm_cat"
+  db_name        = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["db_name"]
   db_hostname    = "lgtm-cat-rds.${local.env}"
 }
 
