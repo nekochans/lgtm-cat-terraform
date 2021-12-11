46b288d1b06d7be9c9bf5143c6d4776dc26f84fc
kobayashi-m42 <m42kobayashi@gmail.com>
2021-12-12 00:38:27 +0900

    rds module に PROD環境用に RDS に接続するための Route53 CNAME レコードを追加

--- modules/aws/rds/route53.tf
+++ modules/aws/rds/route53.tf
@@ -25,3 +25,31 @@ resource "aws_route53_record" "rds_proxy_stg" {
   ttl     = 300
   records = [aws_db_proxy.rds_proxy.endpoint]
 }
+
+resource "aws_route53_zone" "rds" {
+  name = "prod"
+
+  vpc {
+    vpc_id = var.vpc_id
+  }
+
+  comment = "prod RDS Local Domain"
+}
+
+resource "aws_route53_record" "rds" {
+  zone_id = aws_route53_zone.rds.zone_id
+  name    = var.rds_domain_name
+  type    = "CNAME"
+
+  ttl     = 300
+  records = [aws_rds_cluster.rds_cluster.endpoint]
+}
+
+resource "aws_route53_record" "rds_proxy" {
+  zone_id = aws_route53_zone.rds.zone_id
+  name    = var.rds_proxy_domain_name
+  type    = "CNAME"
+
+  ttl     = 300
+  records = [aws_db_proxy.rds_proxy.endpoint]
+}
