1c8522972b52588564fd87e82693022ef7d8e2f5
kobayashi-m42 <m42kobayashi@gmail.com>
2021-12-11 16:47:59 +0900

    STG と PROD 環境で migration 実行環境を分けるために ece-migration module を修正

--- modules/aws/ecs-migration/ssm.tf
+++ modules/aws/ecs-migration/ssm.tf
@@ -1,26 +1,26 @@
 resource "aws_ssm_parameter" "db_hostname" {
-  name      = "/lgtm-cat/migration/DB_HOSTNAME"
+  name      = "/${var.env}/lgtm-cat/migration/DB_HOSTNAME"
   type      = "SecureString"
   value     = var.db_hostname
   overwrite = true
 }
 
 resource "aws_ssm_parameter" "db_username" {
-  name      = "/lgtm-cat/migration/DB_USERNAME"
+  name      = "/${var.env}/lgtm-cat/migration/DB_USERNAME"
   type      = "SecureString"
   value     = var.db_username
   overwrite = true
 }
 
 resource "aws_ssm_parameter" "db_name" {
-  name      = "/lgtm-cat/migration/DB_NAME"
+  name      = "/${var.env}/lgtm-cat/migration/DB_NAME"
   type      = "SecureString"
   value     = var.db_name
   overwrite = true
 }
 
 resource "aws_ssm_parameter" "db_password" {
-  name      = "/lgtm-cat/migration/DB_PASSWORD"
+  name      = "/${var.env}/lgtm-cat/migration/DB_PASSWORD"
   type      = "SecureString"
   value     = var.db_password
   overwrite = true
