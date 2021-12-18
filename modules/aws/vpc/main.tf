77b4d16c18516fdb9d4b1b7f776b86ad879c6f47
kobayashi-m42 <m42kobayashi@gmail.com>
2021-12-18 23:35:34 +0900

    VPC Lambda から S3 にアクセスするための VPC エンドポイントを作成

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -53,3 +53,13 @@ resource "aws_route_table_association" "public" {
     count.index,
   )
 }
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.this.id
+  service_name = "com.amazonaws.ap-northeast-1.s3"
+}
+
+resource "aws_vpc_endpoint_route_table_association" "example" {
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  route_table_id  = aws_route_table.public.id
+}
