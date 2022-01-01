bf6b34f4c3b2492b7f35d8df27ec4ab1b3153b18
kobayashi-m42 <m42kobayashi@gmail.com>
2022-01-01 15:45:19 +0900

    stg に合わせて output を追加

--- providers/aws/environments/prod/12-images/outputs.tf
+++ providers/aws/environments/prod/12-images/outputs.tf
@@ -5,3 +5,7 @@ output "upload_images_bucket_name" {
 output "lgtm_images_bucket_name" {
   value = module.images.lgtm_images_bucket_name
 }
+
+output "lgtm_images_cdn_domain" {
+  value = local.lgtm_images_cdn_domain
+}
