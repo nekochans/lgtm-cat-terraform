9eeda1d0c94e02e222dad131bdc127b3163b5557
kobayashi-m42 <m42kobayashi@gmail.com>
2022-03-28 00:02:08 +0900

    terraform-init.sh のディレクトリ一覧をshell内部で取得するように変更

--- terraform-init.sh
+++ terraform-init.sh
@@ -1,30 +1,13 @@
 #!/bin/sh
 
-tfstateDirList='
-/data/providers/aws/environments/prod/10-network
-/data/providers/aws/environments/prod/11-acm
-/data/providers/aws/environments/prod/12-images
-/data/providers/aws/environments/prod/13-vercel
-/data/providers/aws/environments/prod/14-txt
-/data/providers/aws/environments/prod/15-iam
-/data/providers/aws/environments/prod/16-ses
-/data/providers/aws/environments/prod/17-cognito
-/data/providers/aws/environments/prod/20-api
-/data/providers/aws/environments/prod/21-lambda-securitygroup
-/data/providers/aws/environments/prod/22-migration
-/data/providers/aws/environments/prod/23-rds
-/data/providers/aws/environments/stg/11-acm
-/data/providers/aws/environments/stg/12-images
-/data/providers/aws/environments/stg/17-cognito
-/data/providers/aws/environments/stg/20-api
-/data/providers/aws/environments/stg/21-lambda-securitygroup
-/data/providers/aws/environments/stg/22-migration
-'
+currentDir=$(pwd)
 
-for tfstateDir in ${tfstateDirList}; do
+for tfstateDir in $(find . -maxdepth 5 -regex "\./providers.*/[0-9]\{2,\}.*" -type d); do
   echo "$tfstateDir"
   # shellcheck disable=SC2164
   cd "$tfstateDir"
   pwd
   terraform init
+  # shellcheck disable=SC2164
+  cd "$currentDir"
 done
