a62554b0a201b9268c59ddebc5c8045d6972fcba
keitakn <keita.koga.work@gmail.com>
2022-04-22 11:41:03 +0900

    :sparkles: #36 本番環境にlgtm-cat-image-recognition-apiのリソースを追加

--- providers/aws/environments/prod/17-cognito/variables.tf
+++ providers/aws/environments/prod/17-cognito/variables.tf
@@ -1,9 +1,11 @@
 locals {
-  env                                     = "prod"
-  user_pool_name                          = "${local.env}-lgtmeow-user-pool"
-  user_pool_domain_name                   = "${local.env}-lgtmeow"
-  email_identity_arn                      = data.terraform_remote_state.ses.outputs.email_identity_arn
-  lgtm_cat_api_resource_server_name       = "${local.env}-lgtm-cat-api"
-  lgtm_cat_api_resource_server_identifier = "api.lgtmeow"
-  lgtm_cat_bff_client_name                = "lgtmeow-bff"
+  env                                                       = "prod"
+  user_pool_name                                            = "${local.env}-lgtmeow-user-pool"
+  user_pool_domain_name                                     = "${local.env}-lgtmeow"
+  email_identity_arn                                        = data.terraform_remote_state.ses.outputs.email_identity_arn
+  lgtm_cat_api_resource_server_name                         = "${local.env}-lgtm-cat-api"
+  lgtm_cat_api_resource_server_identifier                   = "api.lgtmeow"
+  lgtm_cat_image_recognition_api_resource_server_name       = "${local.env}-lgtm-cat-image-recognition-api"
+  lgtm_cat_image_recognition_api_resource_server_identifier = "image-recognition-api.lgtmeow"
+  lgtm_cat_bff_client_name                                  = "lgtmeow-bff"
 }
