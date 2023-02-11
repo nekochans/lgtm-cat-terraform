8a4c5fb0dc29274be143cb3ca0bfaf2d478370db
kobayashi-m42 <m42kobayashi@gmail.com>
2023-02-11 15:48:03 +0900

    GitHub Actions 用の OpenID Connect Provider を追加

--- providers/aws/environments/prod/15-iam/main.tf
+++ providers/aws/environments/prod/15-iam/main.tf
@@ -2,3 +2,7 @@ module "iam" {
   source                      = "../../../../../modules/aws/iam"
   serverless_deploy_user_name = local.serverless_deploy_user_name
 }
+
+module "identity_provider" {
+  source = "../../../../../modules/aws/iam/identity-provider"
+}
