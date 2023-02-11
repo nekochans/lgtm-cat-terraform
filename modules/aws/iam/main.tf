f0d1a43ec08697a14d2cd2ec862d2872ba047f1c
kobayashi-m42 <m42kobayashi@gmail.com>
2023-02-11 18:35:29 +0900

    path.module を利用する形にリファクタリング

--- modules/aws/iam/main.tf
+++ modules/aws/iam/main.tf
@@ -6,7 +6,7 @@ resource "aws_iam_user" "serverless_deploy_user" {
 resource "aws_iam_policy" "serverless_deploy_policy" {
   name        = "${var.serverless_deploy_user_name}-policy"
   description = "${var.serverless_deploy_user_name}-policy"
-  policy      = file("../../../../../modules/aws/iam/files/policy/serverless-deploy-policy.json")
+  policy      = file("${path.module}/files/policy/serverless-deploy-policy.json")
 }
 
 resource "aws_iam_user_policy_attachment" "serverless_deploy_policy_attach" {
