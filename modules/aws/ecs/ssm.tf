f3746c41ca1c0e7dd710c53ec3802c157444380d
kobayashi-m42 <m42kobayashi@gmail.com>
2023-06-07 22:04:50 +0900

    resource/aws_ssm_parameterのoverwriteが廃止予定のため削除

--- modules/aws/ecs/ssm.tf
+++ modules/aws/ecs/ssm.tf
@@ -1,41 +1,35 @@
 resource "aws_ssm_parameter" "db_hostname" {
-  name      = "/${var.env}/lgtm-cat/api/DB_HOSTNAME"
-  type      = "SecureString"
-  value     = var.db_hostname
-  overwrite = true
+  name  = "/${var.env}/lgtm-cat/api/DB_HOSTNAME"
+  type  = "SecureString"
+  value = var.db_hostname
 }
 
 resource "aws_ssm_parameter" "db_username" {
-  name      = "/${var.env}/lgtm-cat/api/DB_USERNAME"
-  type      = "SecureString"
-  value     = var.db_username
-  overwrite = true
+  name  = "/${var.env}/lgtm-cat/api/DB_USERNAME"
+  type  = "SecureString"
+  value = var.db_username
 }
 
 resource "aws_ssm_parameter" "db_name" {
-  name      = "/${var.env}/lgtm-cat/api/DB_NAME"
-  type      = "SecureString"
-  value     = var.db_name
-  overwrite = true
+  name  = "/${var.env}/lgtm-cat/api/DB_NAME"
+  type  = "SecureString"
+  value = var.db_name
 }
 
 resource "aws_ssm_parameter" "db_password" {
-  name      = "/${var.env}/lgtm-cat/api/DB_PASSWORD"
-  type      = "SecureString"
-  value     = var.db_password
-  overwrite = true
+  name  = "/${var.env}/lgtm-cat/api/DB_PASSWORD"
+  type  = "SecureString"
+  value = var.db_password
 }
 
 resource "aws_ssm_parameter" "sentry_dsn" {
-  name      = "/${var.env}/lgtm-cat/api/SENTRY_DSN"
-  type      = "SecureString"
-  value     = var.sentry_dsn
-  overwrite = true
+  name  = "/${var.env}/lgtm-cat/api/SENTRY_DSN"
+  type  = "SecureString"
+  value = var.sentry_dsn
 }
 
 resource "aws_ssm_parameter" "cognito_user_pool_id" {
-  name      = "/${var.env}/lgtm-cat/api/COGNITO_USER_POOL_ID"
-  type      = "SecureString"
-  value     = var.cognito_user_pool_id
-  overwrite = true
+  name  = "/${var.env}/lgtm-cat/api/COGNITO_USER_POOL_ID"
+  type  = "SecureString"
+  value = var.cognito_user_pool_id
 }
