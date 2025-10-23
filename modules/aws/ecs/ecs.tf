5cf0c70046d248970ea65df23bef3464ba74404a
kobayashi-m42 <m42kobayashi@gmail.com>
2025-10-23 21:23:30 +0900

    Python版API (v2) リソースを追加

--- modules/aws/ecs/ecs.tf
+++ modules/aws/ecs/ecs.tf
@@ -39,3 +39,36 @@ resource "aws_ecs_service" "api" {
 
   depends_on = [aws_alb_listener.api_public]
 }
+
+resource "aws_ecs_service" "api_v2" {
+  name             = "${var.name}-v2"
+  cluster          = aws_ecs_cluster.api.id
+  // タスクの定義は Terraform ではなく lgtm-cat-api リポジトリで管理
+  task_definition  = ""
+  desired_count    = var.ecs_service_desired_count
+  launch_type      = "FARGATE"
+  platform_version = "1.4.0"
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.api_v2_public.id
+    container_name   = "nginx"
+    container_port   = 80
+  }
+
+  network_configuration {
+    subnets          = var.subnet_public_ids
+    assign_public_ip = true
+
+    security_groups = [
+      aws_security_group.api_ecs.id,
+    ]
+  }
+
+  lifecycle {
+    ignore_changes = [
+      task_definition,
+    ]
+  }
+
+  depends_on = [aws_alb_listener.api_public]
+}
