0ac104173d808081fd01ae35ff86d5ad2a9b9630
kobayashi-m42 <m42kobayashi@gmail.com>
2025-10-29 07:44:51 +0900

    Python版 lgtm-cat-api への移行用リソース(v2)の削除

--- modules/aws/ecs/alb.tf
+++ modules/aws/ecs/alb.tf
@@ -26,8 +26,6 @@ resource "aws_alb_target_group" "api_public" {
 
 resource "aws_alb_listener" "api_public" {
   default_action {
-    # python版に切り替える際に、以下に変更する
-    # target_group_arn = aws_alb_target_group.api_v2_public
     target_group_arn = aws_alb_target_group.api_public.id
     type             = "forward"
   }
@@ -37,37 +35,4 @@ resource "aws_alb_listener" "api_public" {
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-Ext-2018-06"
   certificate_arn   = var.certificate_arn
-}
-
-resource "aws_alb_target_group" "api_v2_public" {
-  name        = "${var.name}-v2"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
-
-  health_check {
-    path                = "/health-checks"
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-    interval            = 20
-    matcher             = 200
-  }
-}
-
-resource "aws_lb_listener_rule" "api_public_preview" {
-  listener_arn = aws_alb_listener.api_public.id
-  priority     = 10
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_alb_target_group.api_v2_public.id
-  }
-
-  condition {
-    path_pattern {
-      values = ["/v2/*"]
-    }
-  }
 }
